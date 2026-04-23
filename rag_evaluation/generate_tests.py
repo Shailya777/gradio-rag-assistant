cfe2c277787c4e4758b77b7640a12caf7594e3f5
Shailya777 <shailya.gandhi100@gmail.com>
2026-04-23 13:07:05 +0530

    changed number of tests to generate

--- rag_evaluation/generate_tests.py
+++ rag_evaluation/generate_tests.py
@@ -12,7 +12,7 @@ load_dotenv(override= True)
 MODEL= 'ollama/llama3.1:8b'
 KNOWLEDGE_BASE_PATH = Path(__file__).parent.parent / 'knowledge-base'
 TEST_FILE_PATH= Path(__file__).parent / 'tests.jsonl'
-NUM_TESTS_TO_GENERATE = 100
+NUM_TESTS_TO_GENERATE = 25
 
 class TestItem(BaseModel):
     """
