e389d4f7d4003604f7600c37d3718785456a4d9b
Shailya777 <shailya.gandhi100@gmail.com>
2026-04-23 12:46:03 +0530

    get_random_markdown_chunks: Grabs a random sample of Markdown files from the Knowledge Base.

--- rag_evaluation/generate_tests.py
+++ rag_evaluation/generate_tests.py
@@ -33,3 +33,26 @@ class TestItem(BaseModel):
     category: str = Field(
         description= 'Must be one of: direct_fact, conceptual, code_syntax, troubleshooting'
     )
+
+def get_random_markdown_chunks(num_chunks):
+    """
+    Grabs a random sample of Markdown files from the Knowledge Base.
+    :param num_chunks: Number of chunks to return.
+    :return: Chunks of Markdown files.
+    """
+
+    all_files= list(KNOWLEDGE_BASE_PATH.rglob('*.md'))
+    random_sampled_files = random.sample(all_files, min(num_chunks, len(all_files)))
+
+    chunks = []
+
+    for file in random_sampled_files:
+        with open(file, 'r', encoding="utf-8") as f:
+            text = f.read()
+            chunks.append(text[:2000]) # If the file is huge, grab the first 2000 characters to keep context clean
+
+    return chunks
+
+if __name__ == '__main__':
+    x = get_random_markdown_chunks(2)
+    print(x)
