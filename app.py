af81f23a8e91372f4d1d0e08366e085cb1253bbd
Shailya777 <shailya.gandhi100@gmail.com>
2026-04-23 11:53:06 +0530

    Added context with reply of LLM.

--- app.py
+++ app.py
@@ -6,7 +6,32 @@ def chat(message, history):
     Acts as the bridge between gradio's UI and RAG backend.
     """
 
+    # Getting Answer and Context (Chunks) from LLM:
     reply_text, chunks = answer_question(message, history)
+
+    # Building a Markdown Accordion to Display Context:
+    if chunks:
+        sources_md= "\n\n<details>\n<summary> <b>View Retrieved Sources</b></summary>\n\n"
+
+        # Looping Through found Chunks:
+        for i, chunk in enumerate(chunks):
+            file_name = chunk.metadata.get('source', 'Unknown File')
+            #print(file_name)
+            clean_file_name = file_name.split('/')[-1]
+
+            sources_md += f"**{i+1}. {clean_file_name}**\n"
+
+            # Preview Text from Chunk:
+            preview_text = chunk.page_content[:150].replace('\n', ' ')
+
+            # Adding Preview Text:
+            sources_md += f"> {preview_text}...\n\n"
+
+        sources_md += "</details>"
+
+        # Adding Source Information to LLM's Reply:
+        reply_text += sources_md
+
     return reply_text
 
 # gradio UI:
