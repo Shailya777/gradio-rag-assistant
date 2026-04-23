f5d525402d1bee88de7a2946d791ec7e4526d5c5
Shailya777 <shailya.gandhi100@gmail.com>
2026-04-23 12:32:38 +0530

    Test Item schema.

--- rag_evaluation/generate_tests.py
+++ rag_evaluation/generate_tests.py
@@ -14,3 +14,22 @@ KNOWLEDGE_BASE_PATH = Path(__file__).parent.parent / 'knowledge-base'
 TEST_FILE_PATH= Path(__file__).parent / 'tests.jsonl'
 NUM_TESTS_TO_GENERATE = 100
 
+class TestItem(BaseModel):
+    """
+    Schema for forcing the LLM to output perfect test data.
+    """
+    question: str = Field(
+        description= 'A highly specific question that can be answered by the text.'
+    )
+
+    keywords: list[str] = Field(
+        description= '3-5 critical words that MUST appear in the retrieved context.'
+    )
+
+    reference_answer: str = Field(
+        description= 'The exact, correct answer based ONLY on the text.'
+    )
+
+    category: str = Field(
+        description= 'Must be one of: direct_fact, conceptual, code_syntax, troubleshooting'
+    )
