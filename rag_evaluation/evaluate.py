338b41fa764159ab5be5713530d872cda7ffb7ac
Shailya777 <shailya.gandhi100@gmail.com>
2026-04-24 11:01:57 +0530

    Schema for LLM-as-a-judge evaluation of answer.

--- rag_evaluation/evaluate.py
+++ rag_evaluation/evaluate.py
@@ -14,3 +14,22 @@ JUDGE_MODEL = 'openai/gpt-4.1-nano'
 TEST_FILE = Path(__file__).parent / 'tests.jsonl'
 RESULTS_FILE = Path(__file__).parent / 'evaluation_results.csv'
 
+class AnswerEval(BaseModel):
+    """
+    Schema for LLM-as-a-judge evaluation of answer.
+    """
+    accuracy: int = Field(
+        description= 'Score 1-5: Is the generated answer factually correct compared to the reference answer? 1 is completely wrong, 5 is perfect.'
+    )
+
+    completeness: int = Field(
+        description= 'Score 1-5: Did it include all necessary information from the reference answer?'
+    )
+
+    relevance: int = Field(
+        description= 'Score 1-5: Did it directly answer the prompt without rambling?'
+    )
+
+    feedback: int = Field(
+        description= '1 sentence explaining the scores.'
+    )
